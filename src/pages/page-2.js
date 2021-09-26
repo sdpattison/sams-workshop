569d164ead2c432ab3e6f0c7398d34f8ea4199ae
sdpattison <samueldpattison@gmail.com>
2021-09-26 18:20:53 -0400

    initial commit

---
+++ src/pages/page-2.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const SecondPage = () =>{
+    return(
+        <h1>
+            Welcome from the Second Page
+        </h1>
+    );
+}
+
+export default SecondPage;
